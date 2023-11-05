d7aeaeb48c288696a2aa93c4cb4f2afe80ba63f3
Artem <bam10112002@gmail.com>
2023-11-05 17:30:03 +0300

    refactor code

--- src/main/java/com/artem/analyzer/controllers/FrequencyController.java
+++ src/main/java/com/artem/analyzer/controllers/FrequencyController.java
@@ -5,7 +5,6 @@ import com.artem.analyzer.services.FrequencyService;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/artem/analyzer/services/FrequencyService.java
+++ src/main/java/com/artem/analyzer/services/FrequencyService.java
@@ -5,7 +5,6 @@ import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
