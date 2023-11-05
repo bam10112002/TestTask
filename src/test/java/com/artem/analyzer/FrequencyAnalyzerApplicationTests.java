21a8c47d86dbd787b8f8a4e16439f02131e79a03
Artem <bam10112002@gmail.com>
2023-11-05 16:08:06 +0300

    init

---
+++ src/test/java/com/artem/analyzer/FrequencyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artem.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrequencyAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
